288a55399103d88b3bb9d802e9ac207987b62ada
swar00pduthks <swaroopduthks@gmail.com>
2025-08-02 18:33:14 +0100

    fix: Apply Spotless formatting to test file

--- api/src/test/java/com/nexadata/lineage/NexaLineageApplicationTests.java
+++ api/src/test/java/com/nexadata/lineage/NexaLineageApplicationTests.java
@@ -6,15 +6,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NexaLineageApplicationTests {
 
-    @Test
-    void contextLoads() {
-        // This test verifies that the Spring context loads successfully
-    }
-    
-    @Test
-    void basicTest() {
-        // This test ensures JaCoCo has something to measure
-        String message = "Hello, Nexa Lineage!";
-        assert message.contains("Nexa");
-    }
-} 
+  @Test
+  void contextLoads() {
+    // This test verifies that the Spring context loads successfully
+  }
+
+  @Test
+  void basicTest() {
+    // This test ensures JaCoCo has something to measure
+    String message = "Hello, Nexa Lineage!";
+    assert message.contains("Nexa");
+  }
+}
